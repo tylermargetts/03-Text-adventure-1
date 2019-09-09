09f4516dfa46db229817e57fccbff463f6b890f9
Yee <yeeet@iu.edu>
2019-09-09 16:47:12 -0400

    completing the assignment

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys, logging, json
+
+#check to make sure we are running the right version of Python
+version = (3,7)
+assert sys.version_info >= version, "This script requires at least Python {0}.{1}".format(version[0],version[1])
+
+#turn on logging, in case we have to leave ourselves debugging messages
+logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+
+
+def main():
+    game = {}
+    with open('zork.json') as json_file:
+        game = json.load(json_file)
+    # Your game goes here!
+
+    current = 'WHOUS'
+
+
+    return True
+
+
+
+#if we are running this from the command line, run main
+if __name__ == '__main__':
+	main()
