3632063471e32786cbf690944019d5f612ac98bc
ethan lam <ethan@149-160-218-186.dhcp-bl.indiana.edu>
2019-09-18 16:42:44 -0400

    Completes the Assignment

--- main.py
+++ main.py
@@ -1,52 +1,58 @@
-import sys, logging, json
 
-#check to make sure we are running the right version of Python
+  
+import sys, os, json
+
 version = (3,7)
 assert sys.version_info >= version, "This script requires at least Python {0}.{1}".format(version[0],version[1])
 
-#turn on logging, in case we have to leave ourselves debugging messages
-logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
-def render (world,current_location):
-    '''print out a description of the current location'''
-    room = world[current_location]
-    print(room["name"])
-    print (room["desc"])
-    return True
 
-def check_input():
-    '''request input from the player, validate the input'''
-    user_input = input("What would you like to do?")
-    #do we want to validate?
-    return user_input
 
-def update():
-    '''check if we need to move to a new location, etc.'''
-    return true
+# Game loop functions
+def render(game,current):
+    ''' Displays the current room '''
+
+    print('You are in the ' + game['rooms'][current]['name'])
+    print(game['rooms'][current]['desc'])
+
+def getInput():
+    ''' Asks the user for input and returns a stripped, uppercase version of what they typed '''
+
+    response = input('What would you like to do? ').strip().upper()
+    return response
+
+
+def update(response,game,current):
+    ''' Process the input and update the state of the world '''
+    for e in game['rooms'][current]['exits']:
+        if response == e['verb']:
+            current = e['target']
+    return current
+
 
 
 def main():
-    game = {} #creating a dictionary 
+
+    game = {}
     with open('zork.json') as json_file:
         game = json.load(json_file)
-    # Your game goes here!
 
-    current = 'WHOUS'
-    quit= False 
+    current = 'START'
+
+    quit = False
+
     while not quit:
-        #render the world
-        render(game["rooms"],current)
-         #check for player input
-        user_input = check_input()
-        #Update the state of the world
-       
+
+        render(game,current)
+        response = getInput()
+        current = update(response,game,current)
+
+        if response == 'QUIT':
+            quit = True
 
 
-    return True
 
 
-
-#if we are running this from the command line, run main
 if __name__ == '__main__':
 	main()
+
